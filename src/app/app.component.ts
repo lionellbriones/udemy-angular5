e3903d31854ef23432198be0e9251998c27097f6
Lionell Briones <lionell.c.briones@accenture.com>
2017-12-27 11:45:51 +0800

    Added view selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  feature = 'recipes';
+
+  viewSelected(feature: string) {
+    this.feature = feature;
+  }
 }
