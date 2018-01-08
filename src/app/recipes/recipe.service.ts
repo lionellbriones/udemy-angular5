9142c3a9e4451548374ffc973f697b1f16b71061
Lionell Briones <lionell.c.briones@accenture.com>
2018-01-08 13:41:10 +0800

    Code clean up

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,9 @@
 import { Ingredient } from './../shared/ingredient.model';
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 
+@Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
