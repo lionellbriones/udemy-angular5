72d828c2bae97a9097a47db2ed15f101bb8ea0df
Lionell Briones <llenoil@gmail.com>
2018-01-14 08:45:03 +0800

    Added new ingredients control

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -58,6 +58,15 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
+
   onSubmit() {
     console.log('Submit');
   }
