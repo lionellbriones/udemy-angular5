9747a7b370c57ba5f2c905e6efca7fd9b7ea7a38
Lionell Briones <lionell.c.briones@accenture.com>
2018-01-15 10:20:41 +0800

    Added signin signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    const config = {
+      apiKey: 'AIzaSyD-4rN0sIhvKqP__aYoVIjjjFp3K9iNzIk',
+      authDomain: 'udemy-test-6546f.firebaseapp.com'
+    };
+
+    firebase.initializeApp(config);
+  }
+}
